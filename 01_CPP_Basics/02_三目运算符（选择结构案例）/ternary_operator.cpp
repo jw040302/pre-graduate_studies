191e6f06012b6f705aaa0d00a626b12284a4552e
jw040302 <jw20040302@gmail.com>
2026-05-31 13:23:15 +0800

    feat: 完成案例“三目运算符”

--- 01_CPP_Basics/02_三目运算符（选择结构案例）/ternary_operator.cpp
+++ 01_CPP_Basics/02_三目运算符（选择结构案例）/ternary_operator.cpp
@@ -1,1 +1,14 @@
 #include <iostream>
+int main() {
+    int a = 10, b = 20, c = 0;
+
+    c = (a > b ? a : b);
+
+    std::cout << "c = "<< c << std::endl;
+
+    (a < b ? a : b) = 100;
+
+    std::cout << "a = " << a << ", b = " << b << std::endl;
+    
+    return 0;
+}
