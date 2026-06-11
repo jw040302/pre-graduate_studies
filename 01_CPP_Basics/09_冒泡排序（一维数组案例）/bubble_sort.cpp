0506b9991c1ca6a9550e12af9ed90b1c9ecd9e56
jw040302 <jw20040302@gmail.com>
2026-06-11 21:58:02 +0800

    feat: 完成数组相关案例

---
+++ 01_CPP_Basics/09_冒泡排序（一维数组案例）/bubble_sort.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+int main() {
+
+  int arr[9] = {4, 2, 8, 0, 5, 7, 1, 3, 9};
+
+  std::cout << "排序前： " << std::endl;
+  for (int i = 0; i < 9; i++) {
+    std::cout << arr[i] << " ";
+  }
+  std::cout << std::endl;
+
+  for (int i = 0; i < 8; i++) {
+    for (int j = 0; j < 8 - i; j++) {
+      if (arr[j] > arr[j + 1]) {
+        int temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
+    }
+  }
+
+  std::cout << "排序后： " << std::endl;
+  for (int i = 0; i < 9; i++) {
+    std::cout << arr[i] << " ";
+  }
+  std::cout << std::endl;
+
+  return 0;
+}
